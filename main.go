c5fab3ed1c95692b25c3df2130f8f8d808a45642
David Martin <davmarti@redhat.com>
2023-12-16 12:59:23 +0000

    checkpoint k8s prompt

--- main.go
+++ main.go
@@ -11,6 +11,16 @@ import (
 	"strings"
 )
 
+type InternalRequest struct {
+	HumanInput     string `json:"human_input"`
+	SystemResponse string `json:"system_response"`
+}
+
+type InternalResponse struct {
+	ResponseToHuman    string `json:"response_to_human"`
+	NextKubectlCommand string `json:"next_kubectl_command"`
+}
+
 type Message struct {
 	Role    string `json:"role"`
 	Content string `json:"content"`
@@ -35,7 +45,7 @@ type ChatGPTResponse struct {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	var lastClientResponse string
-
+	lastClientResponse = ""
 	for {
 		fmt.Print("Enter your message: ")
 		userInput, _ := reader.ReadString('\n')
@@ -48,11 +58,14 @@ func main() {
 		}
 
 		// Include last client response in request if available
-		var messages []Message
-		if lastClientResponse != "" {
-			messages = append(messages, Message{Role: "system", Content: lastClientResponse})
+		internalReq := InternalRequest{
+			HumanInput:     userInput,
+			SystemResponse: lastClientResponse,
 		}
-		messages = append(messages, Message{Role: "user", Content: userInput})
+		internalReqJSON, _ := json.Marshal(internalReq)
+
+		var messages []Message
+		messages = append(messages, Message{Role: "user", Content: string(internalReqJSON)})
 
 		response, err := getChatGPTResponse(messages)
 		if err != nil {
@@ -61,19 +74,22 @@ func main() {
 		}
 
 		fmt.Println("Response:", response)
-
-		if strings.HasPrefix(response, "kubectl") {
-			lastClientResponse = executeKubectlCommand(response)
-		} else {
-			lastClientResponse = ""
-		}
-
+		lastClientResponse = response
 	}
 }
 
 func getChatGPTResponse(messages []Message) (string, error) {
 	// Include your initial instructions along with the messages
-	initialPrompt := "I am using an automated system to manage a Kubernetes cluster. For any action that needs to be taken, please respond with a command that starts directly with 'kubectl', followed by the specific instructions. The response should be concise and formatted for machine execution. For example, if I need to list namespaces, simply reply with 'kubectl get namespaces'. Please avoid additional explanations or text outside the command itself."
+	// initialPrompt := `I am using an automated system with a structured message format.
+	// Each message has two parts: a human input section and a system response section.
+	// The human input section contains commands or questions from a user.
+	// The system response section contains outputs from executing Kubernetes commands.
+	// Please respond with two sections:
+	// 	first, a response to the human input, which can be empty if not necessary;
+	// 	second, a Kubernetes command starting with 'kubectl' for the system to execute, which can also be empty if not necessary.
+	// Please adhere to this structure in your responses.`
+	initialPrompt := `I am using a JSON-based communication protocol for a system that interacts with a Kubernetes cluster. Each message to you will be a JSON object with two fields. The 'human_input' field contains commands or questions from a user about Kubernetes operations. The 'system_response' field contains the output from previously executed Kubernetes commands. Please respond with a JSON object that has two fields: 'response_to_human' and 'next_kubectl_command'. The 'response_to_human' field should contain your direct response to the 'human_input', and the 'next_kubectl_command' field should contain the exact Kubernetes command to be executed next, if any. Ensure that your responses are concise and formatted strictly according to this structure.`
+
 	messages = append([]Message{{Role: "system", Content: initialPrompt}}, messages...)
 
 	url := "https://api.openai.com/v1/chat/completions"
@@ -94,6 +110,7 @@ func getChatGPTResponse(messages []Message) (string, error) {
 	}
 
 	// Create a new request
+	fmt.Printf("RAW REQUEST: %s\n\n", reqBody)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
 	if err != nil {
 		return "", err
@@ -120,9 +137,23 @@ func getChatGPTResponse(messages []Message) (string, error) {
 	if err := json.NewDecoder(resp.Body).Decode(&chatResponse); err != nil {
 		return "", err
 	}
+	fmt.Printf("RAW RESPONSE: %v\n\n", chatResponse)
 
 	if len(chatResponse.Choices) > 0 {
-		return chatResponse.Choices[0].Message.Content, nil
+		var internalResp InternalResponse
+		err := json.Unmarshal([]byte(chatResponse.Choices[0].Message.Content), &internalResp)
+		if err != nil {
+			fmt.Println("Error parsing response:", err)
+		}
+
+		fmt.Println("Response to Human:", internalResp.ResponseToHuman)
+		if internalResp.NextKubectlCommand != "" {
+			fmt.Println("Executing kubectl command:", internalResp.NextKubectlCommand)
+			// Execute kubectl command and update previousSystemResponse
+			return executeKubectlCommand(internalResp.NextKubectlCommand), nil
+		} else {
+			return "", nil
+		}
 	}
 
 	return "", fmt.Errorf("no response from API")
@@ -135,9 +166,9 @@ func executeKubectlCommand(command string) string {
 
 	if err != nil {
 		fmt.Println("Error executing command:", err)
-		return "CLIENT Error: " + err.Error()
+		return "Error: " + err.Error()
 	}
 
 	fmt.Println("Command output:", string(output))
-	return "CLIENT " + string(output)
+	return string(output)
 }
